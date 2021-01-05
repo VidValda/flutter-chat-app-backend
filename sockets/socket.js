f6b10be96d354741819ce56f8ad9a864c345c537
Vid <valdajosevid@gmail.com>
2021-01-05 18:13:10 -0400

    last changes

--- sockets/socket.js
+++ sockets/socket.js
@@ -8,7 +8,7 @@ io.on('connection', client => {
     });
     client.on("mensaje",(payload)=>{
         console.log("Mensaje",payload);
-        io.emit("mensaje", {admin: "nuevo mensaje"})
+        io.emit("mensaje", {admin: "Nuevo mensaje"})
     })
   });
 
